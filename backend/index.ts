7496a48b6721ee23606cfb3013677d73d31437fb
Tung <tbdoan@ucsd.edu>
2021-02-18 22:41:17 -0800

    yeet

--- backend/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import routes from "./routes";
-const app = express();
-const port = 9000;
-
-app.use("/", routes);
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
--- backend/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-
-const router = Router();
-router.get("/", (req, res) => {
-  res.send({ msg: "online" });
-});
-
-export default router;
