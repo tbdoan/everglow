fdda089575a9d4c2af3060fed6192a24082e688d
Tung <tbdoan@ucsd.edu>
2021-03-01 04:44:08 -0800

    revised bg css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-import './App.css';
-import Main from './View/Screens/Main.js'
-import History from './View/Screens/History.js'
-import background from './View/Images/mainbackground.jpg'
-import Settings from './View/Screens/Settings.js'
+import "./App.css";
+import Main from "./View/Screens/Main.js";
+import History from "./View/Screens/History.js";
+import background from "./View/Images/mainbackground.jpg";
+import Settings from "./View/Screens/Settings.js";
 
 function App() {
   return (
-    <div className = "wholeAssApp">
-      <div><History /></div>
-      <div><Settings /></div>
-      <div><Main /></div>
+    <div className="wholeAssApp">
+      <div>
+        <History />
+      </div>
+      <div>
+        <Settings />
+      </div>
+      <div>
+        <Main />
+      </div>
     </div>
   );
 }
