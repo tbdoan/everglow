e2913616d00908df146b89b3bc4b6c20f88a1ca9
Tung <tbdoan@ucsd.edu>
2021-03-01 04:04:17 -0800

    css navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
-import './App.css';
-import Main from './View/Screens/Main.js'
-import SettingsButton from './View/Components/SettingsButton'
-import HistoryButton from './View/Components/HistoryButton'
-import History from './View/Screens/History.js'
-import background from './View/Images/mainbackground.jpg'
-import Settings from './View/Screens/Settings.js'
+import "./App.css";
+import Main from "./View/Screens/Main.js";
+import SettingsButton from "./View/Components/SettingsButton";
+import HistoryButton from "./View/Components/HistoryButton";
+import History from "./View/Screens/History.js";
+import background from "./View/Images/mainbackground.jpg";
+import Settings from "./View/Screens/Settings.js";
 
 function App() {
   return (
     <div>
-      <div><History /></div>
-      <div><Settings /></div>
-      <div style={{ backgroundImage: `url(${background})` }}><Main /></div>
+      <History />
+      <Settings />
+      <div style={{ backgroundImage: `url(${background})` }}>
+        <Main />
+      </div>
     </div>
   );
 }
--- frontend/src/View/Screens/Main.js
+++ frontend/src/View/Screens/Main.js
@@ -1,25 +1,24 @@
 // Imports
-import temp_plant from '../Images/temp_plant.jpg'
-import './Main.css';
-
+import temp_plant from "../Images/temp_plant.jpg";
+import "./Main.css";
 
 function Main() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <h1> Plamp Dashboard </h1>
-          </header>
-          <body>
-            <table class = "mainTable">
-              <tr class = "mainRow">
-                 <td>on off</td> 
-                 <td>the plant emoji thing</td> 
-                 <td>battery icon thing</td> 
-              </tr>
-            </table>
-          </body>
-        </div>
-      );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 id="title"> Plamp Dashboard </h1>
+      </header>
+      <body>
+        <table class="mainTable">
+          <tr class="mainRow">
+            <td>on off</td>
+            <td>the plant emoji thing</td>
+            <td>battery icon thing</td>
+          </tr>
+        </table>
+      </body>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
