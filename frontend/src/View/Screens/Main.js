c28200ea011614e750e4cd1d5a6c214f5ef1a7d4
Tung <tbdoan@ucsd.edu>
2021-03-03 08:58:06 -0800

    ooof

--- frontend/src/View/Screens/Main.js
+++ frontend/src/View/Screens/Main.js
@@ -1,31 +1,36 @@
 // Imports
-import temp_plant from '../Images/temp_plant.jpg'
-import PlantDisplay from '../Components/PlantDisplay'
-import background from '../../View/Images/mainbackground.jpg'
-import './Main.css';
-import BatteryDisplay from '../Components/BatteryDisplay.js';
-import OnOffDisplay from '../Components/OnOffDisplay.js';
-import NameDisplay from '../Components/NameDisplay.js';
+import PlantDisplay from "../Components/PlantDisplay";
+import "./Main.css";
+import "../../base.css";
+import "../../basic-grid.css";
+import BatteryDisplay from "../Components/BatteryDisplay.js";
+import OnOffDisplay from "../Components/OnOffDisplay.js";
+import NameDisplay from "../Components/NameDisplay.js";
 
 function Main() {
-  
-    return (
-        <div className="App">
-          <header className="App-header">
-            <h1 id="title"> Everglow Dashboard </h1>
-          </header>
-          <body>
-            <p className="name"> <NameDisplay /> </p>
-            <table class = "mainTable">
-              <tr class = "mainRow">
-                 <td className= "leftbox"> <OnOffDisplay/> </td> 
-                 <td className="plantBox"> <PlantDisplay /> </td> 
-                 <td className = "rightBox"><BatteryDisplay/></td> 
-              </tr>
-            </table>
-          </body>
-        </div>
-      );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 id="title"> Everglow Dashboard </h1>
+      </header>
+      <body>
+        <p className="name">
+          <NameDisplay />
+        </p>
+        <section className="basic-grid">
+          <div className="card">
+            <OnOffDisplay />
+          </div>
+          <div className="card">
+            <PlantDisplay />
+          </div>
+          <div className="card">
+            <BatteryDisplay />
+          </div>
+        </section>
+      </body>
+    </div>
+  );
 }
 
 export default Main;
