23ad3fb97661175fd7234ea07df9ccf75550399d
Laurent Lee <laurentl1212@gmail.com>
2021-03-02 18:12:24 -0800

    Created Solar Sensor

--- frontend/src/View/Screens/Main.js
+++ frontend/src/View/Screens/Main.js
@@ -4,6 +4,7 @@ import PlantDisplay from '../Components/PlantDisplay'
 import background from '../../View/Images/mainbackground.jpg'
 import './Main.css';
 import BatteryDisplay from '../Components/BatteryDisplay.js';
+import SolarSensorDisplay from '../Components/SolarSensorDisplay.js';
 import OnOffDisplay from '../Components/OnOffDisplay.js';
 import NameDisplay from '../Components/NameDisplay.js';
 import React, { useState, useEffect, useRef } from "react";
@@ -48,7 +49,7 @@ function Main() {
             <p className="name"> <NameDisplay /> </p>
             <table class = "mainTable">
               <tr class = "mainRow">
-                 <td className= "leftbox"> {volt}
+                 <td className= "leftbox"> <SolarSensorDisplay volt ={volt}/>
                  </td> 
                  <td className="plantBox"> <PlantDisplay /> </td> 
                  <td className = "rightBox"><BatteryDisplay/></td> 
